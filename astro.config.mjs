0bb027de35eaf79d3bc0a5e7204e279451c78495
Karol Binkowski <starystachu9@gmail.com>
2024-08-10 16:10:06 +0200

    build: add vue package, remove @eslint/js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 import pageInsight from 'astro-page-insight';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), pageInsight(), sitemap()],
+  integrations: [tailwind(), pageInsight(), sitemap(), vue()],
 });
