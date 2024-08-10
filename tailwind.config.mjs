e4fa5b826867532c46450a15f5f7b17d1e6aa28f
Karol Binkowski <starystachu9@gmail.com>
2024-08-10 18:24:28 +0200

    refactor: remove shared styles from tailwind config
    I used dynamic values everywhere, so I prefer not to mix things up.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,23 +4,8 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          500: '#2D473E',
-          700: '#2D473E',
-          800: '#293632',
-        },
-        secondary: {
-          200: '#808080',
-        },
-      },
-      borderRadius: {
-        input: '0.625rem',
-      },
-      fontFamily: {
-        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      sans: ['Roboto', ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
