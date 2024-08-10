43c8f07e15ad16a9abdf48a07d2a895b3fb649c1
Karol Binkowski <starystachu9@gmail.com>
2024-08-10 16:13:56 +0200

    feat: add hero section with image carousel

---
+++ src/components/hero/HeroImageGallery.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import featuredImage from '/public/images/hero/1.png';
+import alternative1 from '/public/images/hero/2.png';
+import alternative2 from '/public/images/hero/3.png';
+import alternative3 from '/public/images/hero/4.png';
+
+const images = ref([alternative1, alternative2, alternative3, featuredImage]);
+
+const handleChangeActiveImage = (index: number) => {
+  const selectedElement = images.value[index];
+
+  images.value = [
+    ...images.value.slice(0, index),
+    ...images.value.slice(index + 1),
+    selectedElement,
+  ];
+};
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <img
+      :src="images[3].src"
+      alt=""
+      :width="images[0].width"
+      :height="images[0].height"
+      loading="eager"
+    />
+
+    <div class="max-w-[30.375rem] h-auto flex flex-wrap gap-10 justify-between">
+      <img
+        v-for="(image, index) in images.slice(0, 3)"
+        :key="image.src"
+        class="rounded-lg border w-[8.1875rem] h-[8.1875rem]"
+        :src="image.src"
+        alt=""
+        :width="image.width"
+        :height="image.height"
+        loading="eager"
+        @click="() => handleChangeActiveImage(index)"
+      />
+    </div>
+  </div>
+</template>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,19 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          500: '#2D473E',
+          700: '#2D473E',
+          800: '#293632',
+        },
+        secondary: {
+          200: '#808080',
+        },
+      },
+      borderRadius: {
+        input: '10px',
+      },
       fontFamily: {
         sans: ['Roboto', ...defaultTheme.fontFamily.sans],
       },
